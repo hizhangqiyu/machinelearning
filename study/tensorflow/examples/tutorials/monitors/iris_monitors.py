1e7afe60a080e0a69dfae8f8c6390313db1e0884
AppleFairy <zqyjixiang89@gmail.com>
2016-11-30 16:08:22 +0800

    update comments

--- study/tensorflow/examples/tutorials/monitors/iris_monitors.py
+++ study/tensorflow/examples/tutorials/monitors/iris_monitors.py
@@ -33,6 +33,13 @@ training_set = tf.contrib.learn.datasets.base.load_csv_with_header(
 test_set = tf.contrib.learn.datasets.base.load_csv_with_header(
     filename=IRIS_TEST, target_dtype=np.int, features_dtype=np.float)
 
+# ALL:  all the sample
+# TP :  Positive predict to Positive
+# FP :  Positive predict to Negative
+# FN :  Negative predict to Positive  
+# accuracy  = TP / ALL
+# precision = TP / (TP + FP)
+# recall    = TP / (TP + FN)
 validation_metrics = {"accuracy": tf.contrib.metrics.streaming_accuracy,
                       "precision": tf.contrib.metrics.streaming_precision,
                       "recall": tf.contrib.metrics.streaming_recall}
