70462ae1450789cb9cf4161d1874e1b0fbbcaefb
AppleFairy <zqyjixiang89@gmail.com>
2018-01-28 17:39:17 +0800

    fix code indent issue

--- framework_study/tensorflow/tutorial_r1.4/convolutional_neural_networks/cifar10_input.py
+++ framework_study/tensorflow/tutorial_r1.4/convolutional_neural_networks/cifar10_input.py
@@ -52,22 +52,22 @@ def _generate_image_and_label_batch(image, label, min_queue_examples, batch_size
             min_after_dequeue=min_queue_examples)
     else:
         images, label_batch = tf.train.batch(
-        	[image, label],
-        	batch_size=batch_size,
-        	num_threads=num_preprocess_threads,
-        	capacity=min_queue_examples + 3 * batch_size)
+            [image, label],
+            batch_size=batch_size,
+            num_threads=num_preprocess_threads,
+            capacity=min_queue_examples + 3 * batch_size)
 
     tf.summary.image('images', images)
 
     return images, tf.reshape(label_batch, [batch_size])
 
 def distorted_inputs(data_dir, batch_size):
-	# cifar-10 has 5 data files.
-	filenames = [op.path.join(data_dir, 'data_batch_%d.bin' % i for i in range(1,6))]
+     # cifar-10 has 5 data files.
+    filenames = [op.path.join(data_dir, 'data_batch_%d.bin' % i for i in range(1,6))]
 
     for f in filenames:
-    	if not tf.gfile.Exists(f):
-    		raise ValueError('Failed to find file:' + f)
+        if not tf.gfile.Exists(f):
+            raise ValueError('Failed to find file:' + f)
 
     filename_queue = tf.train.string_input_producer(filenames)
 
@@ -79,52 +79,52 @@ def distorted_inputs(data_dir, batch_size):
 
     distorted_image = tf.random_crop(reshaped_image, [height, width, 3])
     distorted_image = tf.image.random_flip_left_right(distorted_image)
-	# ?????
-	distorted_image = tf.image.random_brightness(distorted_image, max_delta=63)
-  	distorted_image = tf.image.random_contrast(distorted_image, lower=0.2, upper=1.8)
+    # ?????
+    distorted_image = tf.image.random_brightness(distorted_image, max_delta=63)
+    distorted_image = tf.image.random_contrast(distorted_image, lower=0.2, upper=1.8)
 
-  	float_image = tf.image.per_image_standardization(distorted_image)
+    float_image = tf.image.per_image_standardization(distorted_image)
 
-  	float_image.set_shape([height, width, 3])
-  	read_imput.label.set_shape([1])
+    float_image.set_shape([height, width, 3])
+    read_imput.label.set_shape([1])
 
-  	min_fraction_of_examples_in_queue = 0.4
-  	min_queue_examples = int(NUM_EXAMPLES_PRE_EPOCH_FOR_TRAIN * min_fraction_of_examples_in_queue)
-  	
-  	print ('Filling queue with %d CIFAR images before starting to train. '
+    min_fraction_of_examples_in_queue = 0.4
+    min_queue_examples = int(NUM_EXAMPLES_PRE_EPOCH_FOR_TRAIN * min_fraction_of_examples_in_queue)
+      
+    print ('Filling queue with %d CIFAR images before starting to train. '
          'This will take a few minutes.' % min_queue_examples)
 
-  	return _generate_image_and_label_batch(floa_image,
-  	 	read_input.label, min_queue_examples, batch_size, shuffle=True)
+    return _generate_image_and_label_batch(floa_image,
+        read_input.label, min_queue_examples, batch_size, shuffle=True)
 
 def inputs(eval_data, data_dir, batch_size):
-	if not eval_data:
-		filenames = [os.path.join(data_dir, 'data_batch_%d.bin' % i for i in range(1, 6))]
-		num_examples_pre_epoch = NUM_EXAMPLES_PRE_EPOCH_FOR_TRAIN
-	else:
-		filenames = [os.path.join(data_dir, 'test_batch.bin')]
-		num_examples_pre_epoch = NUM_EXAMPLES_PRE_EPOCH_FOR_EVAL
+    if not eval_data:
+        filenames = [os.path.join(data_dir, 'data_batch_%d.bin' % i for i in range(1, 6))]
+        num_examples_pre_epoch = NUM_EXAMPLES_PRE_EPOCH_FOR_TRAIN
+    else:
+        filenames = [os.path.join(data_dir, 'test_batch.bin')]
+        num_examples_pre_epoch = NUM_EXAMPLES_PRE_EPOCH_FOR_EVAL
 
-	for f in filemames:
-		if not tf.gfile.Exists(f):
-			raise ValueError('Failed to find file: ' + f)
+    for f in filemames:
+        if not tf.gfile.Exists(f):
+            raise ValueError('Failed to find file: ' + f)
 
-	filename_queue = tf.train.string_input_producer(filenames)
+    filename_queue = tf.train.string_input_producer(filenames)
 
-	read_input = read_cifar10(filename_queue)
-	reshape_image = tf.cast(read_input.uint8image, tf.float32)
+    read_input = read_cifar10(filename_queue)
+    reshape_image = tf.cast(read_input.uint8image, tf.float32)
 
-	height = IMAGE_SIZE
-	width = IMAGE_SIZE
+    height = IMAGE_SIZE
+    width = IMAGE_SIZE
 
-	resized_image = tf.image.resize_image_with_crop_or_pad(reshape_image, height, width)
+    resized_image = tf.image.resize_image_with_crop_or_pad(reshape_image, height, width)
 
-	float_image = tf.image.per_image_standardization(resized_image)
+    float_image = tf.image.per_image_standardization(resized_image)
 
-	float_image.set_shape([height, width, 3])
-	read_input.label.set_shape([1])
-	min_fraction_of_examples_in_queue = 0.4
-	min_queue_examples = int(num_examples_pre_epoch * min_fraction_of_examples_in_queue)
+    float_image.set_shape([height, width, 3])
+    read_input.label.set_shape([1])
+    min_fraction_of_examples_in_queue = 0.4
+    min_queue_examples = int(num_examples_pre_epoch * min_fraction_of_examples_in_queue)
 
-	return _generate_image_and_label_batch(float_image, read_input.label,
-		min_queue_examples, batch_size, shuffle=False)
+    return _generate_image_and_label_batch(float_image, read_input.label,
+        min_queue_examples, batch_size, shuffle=False)
